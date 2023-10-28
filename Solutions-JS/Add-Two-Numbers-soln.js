a22422753681a513947f4e1152f65652e2cafbbd
preston176 <prestonnyamweya@gmail.com>
2023-10-28 18:42:24 +0300

    add 2 numbers solution

---
+++ Solutions-JS/Add-Two-Numbers-soln.js
@@ -0,0 +1,34 @@
+function addTwoNumbers(l1, l2) {
+    let dummyHead = new ListNode(0);
+    let current = dummyHead;
+    let carry = 0;
+
+    while (l1 || l2 || carry) {
+        const val1 = l1 ? l1.val : 0;
+        const val2 = l2 ? l2.val : 0;
+
+        const sum = val1 + val2 + carry;
+        carry = Math.floor(sum / 10);
+
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+
+    return dummyHead.next;
+}
+
+// Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = val || 0;
+    this.next = next || null;
+}
+
+// Example usage:
+const l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+const l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+const result = addTwoNumbers(l1, l2);
+console.log(result);
